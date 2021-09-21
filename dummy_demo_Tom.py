67b325eba4a7c5e1b9e93862c72ef4a1623fcbec
hhiiddee1 <hidde.van.oijen@gmail.com>
2021-09-21 11:40:01 +0200

    Create dummy_demo_Tom.py

---
+++ dummy_demo_Tom.py
@@ -0,0 +1,259 @@
+################################
+# EvoMan FrameWork - V1.0 2016 #
+# Author: Karine Miras         #
+# karine.smiras@gmail.com      #
+################################
+
+# imports framework
+import sys, os
+sys.path.insert(0, 'evoman') 
+from environment import Environment
+from demo_controller import player_controller
+
+# import modules
+import time
+import numpy as np
+from math import fabs,sqrt
+import glob, os
+import random
+
+# import DEAP
+from deap import base, creator
+from deap import tools
+from deap import algorithms
+    
+    
+# choose this for not using visuals and thus making experiments faster
+headless = True
+if headless:
+    os.environ["SDL_VIDEODRIVER"] = "dummy"
+
+experiment_name = 'dummy_demo_Tom'
+if not os.path.exists(experiment_name):
+    os.makedirs(experiment_name)
+    
+n_hidden_neurons = 10
+
+# initializes environment with ai player using random controller, playing against static enemy
+env = Environment(experiment_name=experiment_name,
+                  enemies=[2],
+                  playermode="ai",
+                  player_controller=player_controller(n_hidden_neurons),
+                  enemymode="static",
+                  level=2,
+                  speed="fastest")
+
+# default environment fitness is assumed for experiment
+
+env.state_to_log() # checks environment state
+
+
+####   Optimization for controller solution (best genotype-weights for phenotype-network): Ganetic Algorihm    ###
+
+ini = time.time()  # sets time marker
+
+
+# genetic algorithm params
+
+run_mode = 'train' # train or test
+
+n_vars = (env.get_num_sensors()+1)*n_hidden_neurons + (n_hidden_neurons+1)*5
+
+dom_u = 1
+dom_l = -1
+npop = 10
+gens = 2
+mutation = 0.2
+last_best = 0
+
+def simulation(env,x):
+    f,p,e,t = env.play(pcont=x)
+    return f
+
+# Evaluation function
+def evaluate(x):
+    return np.array(list(map(lambda y: simulation(env,y), x)))
+
+def fitness_f(x):
+    x = np.array(x)
+    return [simulation(env, x)]
+
+def cxTwoPointCopy(ind1, ind2):
+    """Execute a two points crossover with copy on the input individuals. The
+    copy is required because the slicing in numpy returns a view of the data,
+    which leads to a self overwritting in the swap operation. It prevents
+    ::
+    
+        >>> import numpy
+        >>> a = numpy.array((1,2,3,4))
+        >>> b = numpy.array((5,6,7,8))
+        >>> a[1:3], b[1:3] = b[1:3], a[1:3]
+        >>> print(a)
+        [1 6 7 4]
+        >>> print(b)
+        [5 6 7 8]
+    """
+    size = len(ind1)
+    cxpoint1 = random.randint(1, size)
+    cxpoint2 = random.randint(1, size - 1)
+    if cxpoint2 >= cxpoint1:
+        cxpoint2 += 1
+    else: # Swap the two cx points
+        cxpoint1, cxpoint2 = cxpoint2, cxpoint1
+
+    ind1[cxpoint1:cxpoint2], ind2[cxpoint1:cxpoint2] \
+        = ind2[cxpoint1:cxpoint2].copy(), ind1[cxpoint1:cxpoint2].copy()
+        
+    return ind1, ind2
+
+# Tool decorator
+def checkBounds(min, max):
+    def decorator(func):
+        def wrapper(*args, **kargs):
+            offspring = func(*args, **kargs)
+            for child in offspring:
+                for i in range(len(child)):
+                    if child[i] > max:
+                        child[i] = max
+                    elif child[i] < min:
+                        child[i] = min
+            return offspring
+        return wrapper
+    return decorator
+
+# set up DEAP framework
+creator.create("FitnessMax", base.Fitness, weights=(1,))
+creator.create("Individual", np.ndarray, fitness=creator.FitnessMax)
+
+# Initialize DEAP
+toolbox=base.Toolbox()
+IND_SIZE = n_vars
+
+toolbox = base.Toolbox()
+toolbox.register("attribute", np.random.uniform, dom_l, dom_u)
+toolbox.register("individual", tools.initRepeat, creator.Individual,
+                 toolbox.attribute, n=IND_SIZE)
+toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+
+
+toolbox.register("mate", cxTwoPointCopy)
+toolbox.register("mutate", tools.mutGaussian, mu=0, sigma=1, indpb=0.1)
+toolbox.register("select", tools.selTournament, tournsize=3)
+toolbox.register("evaluate", fitness_f)
+toolbox.register("generate", tools.initRepeat, list, toolbox.individual)
+
+toolbox.decorate("mate", checkBounds(dom_l, dom_u))
+toolbox.decorate("mutate", checkBounds(dom_l, dom_u))
+
+stats = tools.Statistics(lambda ind: ind.fitness.values)
+stats.register("avg", np.mean)
+stats.register("std", np.std)
+stats.register("min", np.min)
+stats.register("max", np.max)
+
+
+# loads file with the best solution for testing
+if run_mode =='test':
+
+    bsol = np.loadtxt(experiment_name+'/best.txt')
+    print( '\n RUNNING SAVED BEST SOLUTION \n')
+    env.update_parameter('speed','normal')
+    evaluate([bsol])
+
+    sys.exit(0)
+
+
+# Set up EA frameworks
+def main():
+    # Create population and set evolution parameters
+    pop = toolbox.population(n=5)
+    CXPB, MUTPB, NGEN = 0, 0.1, 5
+    # Evaluate the entire population
+    fitnesses = map(toolbox.evaluate, pop)
+    for ind, fit in zip(pop, fitnesses):
+        ind.fitness.values = (fit,)
+    
+    logbook = tools.Logbook()
+    logbook.header = "gen", "evals", "std", "min", "avg", "max"
+    
+    for g in range(NGEN):
+        # Select the next generation individuals
+        offspring = toolbox.select(pop, len(pop))
+        # Clone the selected individuals
+        offspring = toolbox.clone(offspring)
+    
+        # Apply crossover and mutation on the offspring
+        for child1, child2 in zip(offspring[::2], offspring[1::2]):
+            if random.random() < CXPB:
+                toolbox.mate(child1, child2)
+                del child1.fitness.values
+                del child2.fitness.values
+                
+    
+        for mutant in offspring:
+            if random.random() < MUTPB:
+                toolbox.mutate(mutant)
+                del mutant.fitness.values
+                
+    
+        # Evaluate the individuals with an invalid fitness
+        invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
+        fitnesses = map(toolbox.evaluate, invalid_ind)
+        print('INVALID IND: ', len(invalid_ind))
+        for ind, fit in zip(invalid_ind, fitnesses):
+            ind.fitness.values = (fit,)
+            
+            
+        # The population is entirely replaced by the offspring
+        pop[:] = offspring
+
+    return pop
+
+def main_mu_plus_lambda(MU, LAMBDA, CXPB, MUTPB, NGEN):
+
+    logbook = tools.Logbook()
+    logbook.header = "gen", "evals", "std", "min", "avg", "max"
+    
+    pop = toolbox.population(n=MU)
+    hof = tools.HallOfFame(1, similar = lambda x,y: np.all(x==y))
+    pop, logbook = algorithms.eaMuPlusLambda(pop, toolbox, mu=MU, lambda_=LAMBDA, 
+        cxpb=CXPB, mutpb=MUTPB, ngen=NGEN, stats=stats, halloffame=hof)
+    
+    return pop, logbook, hof
+    
+def main_mu_comma_lambda(MU, LAMBDA, CXPB, MUTPB, NGEN):      
+    
+    logbook = tools.Logbook()
+    logbook.header = "gen", "evals", "std", "min", "avg", "max"
+    
+    pop = toolbox.population(n=MU)
+    hof = tools.HallOfFame(1, similar = lambda x, y: np.all(x==y))
+    pop, logbook = algorithms.eaMuCommaLambda(pop, toolbox, mu=MU, lambda_=LAMBDA, 
+        cxpb=CXPB, mutpb=MUTPB, ngen=NGEN, stats=stats, halloffame=hof)
+    
+    return pop, logbook, hof    
+
+
+def experiment(function, runs):
+    MU, LAMBDA = 20, 20rsu
+    CXPB, MUTPB, NGEN = 0.8, 0.2, 10
+    results = []
+    best = []
+    means = []
+    stds = []
+    for i in range(runs):
+        pop, logbook, hof = function(MU, LAMBDA, CXPB, MUTPB, NGEN)
+        results.append([pop, logbook, hof])
+        #best.append[results[i][2][0].fitness.values[0]]
+
+        
+    return results
+
+results = experiment(main_mu_plus_lambda, 1)
+
+
+
+
+
+
+
